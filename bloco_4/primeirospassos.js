ad760e9a0dba1a1e851f82ddfe96898b03c77f27
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-02-24 19:00:17 -0300

    Ate o exercicio 5

---
+++ bloco_4/primeirospassos.js
@@ -0,0 +1,45 @@
+1
+let a = 5;
+let b = 9;
+
+console.log(a + b)
+console.log(b -a)
+console.log(a * b)
+console.log(b / a)
+console.log(b % a)
+
+2
+let num1 = 27;
+let num2 = 32;
+
+if (num1 >= num2) {
+    console.log(num1)
+} else {
+    console.log(num2)
+}
+
+3
+let num3 = 45;
+let num4 = 38;
+let num5 = 40;
+
+if ( num3 >= num4 && num3 >= num5){
+    console.log(num3)
+} else if (num4 >= num3 && num4 >= num5) {
+    console.log(num4)
+} else {
+    console.log(num5)
+}
+
+4
+let num6 = 0;
+
+if (num6 > 0) {
+    console.log("Positivo");
+} else if (num6 < 0) {
+    console.log("Negativo");
+} else {
+    console.log("Zero");
+}
+
+5
