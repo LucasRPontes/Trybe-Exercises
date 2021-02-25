3490f631ba0a30a147d991bf289e43c232e7fb7c
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-02-25 15:51:04 -0300

    comecando

---
+++ bloco_4/4.2/arrayefor.js
@@ -0,0 +1,20 @@
+let coisas = ["Taco", "Mesa", "Cadeira", "Porta"];
+
+coisas.push("Garfo");
+coisas.unshift("Facas");
+
+console.log(coisas);
+
+
+let nada = ["Computador", "Geladeira", "som", "Tv", "Chuva"];
+
+nada.pop();
+nada.shift();
+
+let indexOf = nada.indexOf("Tv");
+
+console.log(indexOf);
+
+
+Exercícios
+1
