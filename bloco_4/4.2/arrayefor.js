958619b107767ce3d0171e61f2b8687de5383aaf
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-02-26 19:00:35 -0300

    ate o exercicio 7

--- bloco_4/4.2/arrayefor.js
+++ bloco_4/4.2/arrayefor.js
@@ -41,3 +41,91 @@ let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
   for (let i=0; i < groceryList.length; i++) {
     console.log(groceryList[i]);
   }
+
+
+1---------------------------------------------------------
+
+  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+  for (let i=0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+  }
+
+2---------------------------------------------------------
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for (let i=0; i < numbers.length; i++) {
+  soma += numbers[i];
+} 
+
+console.log(soma);
+
+3---------------------------------------------------------
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let i=0; i < numbers.length; i++) {
+  soma += numbers[i];
+} 
+
+soma = (soma / numbers.length);
+
+console.log(soma);
+
+4-------------------------------------------------------------
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let i=0; i < numbers.length; i++) {
+  soma += numbers[i];
+} 
+
+soma = (soma / numbers.length);
+
+if (soma > 20) {
+  console.log("valor maior que 20")
+} else {
+  console.log("valor menor que 20")
+}
+
+5-------------------------------------------------------------
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let higherNumber = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > higherNumber) {
+    higherNumber = numbers[i];
+  }
+}
+
+console.log(higherNumber);
+
+6-------------------------------------------------------------
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numerosImpares = 0;
+  for (let i=0; i < numbers.length; i++) {
+    if (numbers[i] % 2 != 0) {
+      numerosImpares += 1;
+    } 
+  }
+
+  if (numerosImpares === 0) {
+    console.log("nenhum valor encontrado");
+  } else {
+    console.log(numerosImpares);
+  }
+
+  
+
+  7-------------------------------------------------------------
+  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+  let menorNumero = 200;
+  for (let i=0; i < numbers.length; i++) {
+    if (numbers[i] < menorNumero){
+      menorNumero = numbers[i];
+    }
+  }
+
+  console.log(menorNumero);
+
+  8-----------------------------------------------------------
