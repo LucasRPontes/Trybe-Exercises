e9cb9c152a34896476a7abd047d8f645bd6e205f
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-02-26 11:11:11 -0300

    finalizados exercicios 4.1

--- bloco_4/4.1/primeirospassos.js
+++ bloco_4/4.1/primeirospassos.js
@@ -133,4 +133,35 @@ if (custo < 0 || venda < 0) {
 }
 
 11
-let salarioBruto = 3500;
+let salarioBruto = 4000;
+let salarioSemInss;
+let salarioLiquido;
+
+if (salarioBruto <= 1556.94 && salarioBruto > 0) {
+    salarioSemInss = (salarioBruto - (salarioBruto * 0.08));
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+    salarioSemInss = (salarioBruto - (salarioBruto * 0.09));
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+    salarioSemInss = (salarioBruto - (salarioBruto * 0.11));
+} else if (salarioBruto > 5189.82) {
+    salarioSemInss = (salarioBruto - 570.88);
+} else {
+    salarioSemInss = "Valor inválido"
+}
+
+if (salarioSemInss > 0 && salarioSemInss <= 1903.98) {
+    salarioLiquido = salarioSemInss;
+} else if (salarioSemInss >= 1903.99 && salarioSemInss <= 2826.65) {
+    salarioLiquido = (salarioSemInss - ((salarioSemInss * 0.075) - 142.80));
+} else if (salarioSemInss >= 2826.66 && salarioSemInss <= 3751.05) {
+    salarioLiquido = (salarioSemInss - ((salarioSemInss * 0.15) - 354.80));
+} else if (salarioSemInss >= 3751.06 && salarioSemInss <= 4664.68) {
+    salarioLiquido = (salarioSemInss - ((salarioSemInss * 0.225) - 636.13));
+} else if (salarioSemInss >= 4664.69) {
+    salarioLiquido = (salarioSemInss - ((salarioSemInss * 0.275) - 869.36));
+} else {
+    salarioLiquido = "Valor inválido"
+}
+
+console.log(salarioLiquido);
+
